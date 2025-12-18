a1767d60278a86372e181db4558192bd595f1195
Sudesh A. <sudesh.contact@gmail.com>
2025-12-18 14:15:58 +0545

    add home and about text to the header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -4,13 +4,15 @@
             <nav>
                 <ul class="nav-links">
                     <li class="nav-link">
-                        <NuxtLink to="/">
+                        <NuxtLink to="/" class="link-item">
                             <UIcon name="i-heroicons-rectangle-group" />
+                            <span class="link-text">Home</span>
                         </NuxtLink>
                     </li>
                     <li class="nav-link">
-                        <NuxtLink to="/about">
+                        <NuxtLink to="/about" class="link-item">
                             <UIcon name="i-heroicons-user-circle" />
+                            <span class="link-text">About</span>
                         </NuxtLink>
                     </li>
                 </ul>
@@ -19,12 +21,20 @@
         </header>
     </AContainer>
 </template>
+
 <style scoped>
 .nav-links {
     @apply flex justify-between;
 }
 
 .nav-link {
-    @apply flex text-5xl;
+    @apply flex;
 }
-</style>
+
+.link-item {
+    @apply flex flex-col items-center gap-1 text-5xl;
+}
+
+.link-text {
+    @apply text-sm font-medium;
+}
